2cdc9fa85d66cbd026bbf6a6d63f4ce0fd3a062d
RTFMN <nbokaev@gmail.com>
2021-02-08 19:12:18 +0300

    Initial Commit

---
+++ Password Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password Generator
+//
+//  Created by RTFMN on 08.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
